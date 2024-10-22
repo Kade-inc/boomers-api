7e5def62ff661f4d7ecf0a853b0ae05e8fcd2c4e
Paul Otieno <vitalispaul48@live.com>
2024-10-23 02:33:01 +0300

    Update getAllChallenges

--- controllers/challengesController.ts
+++ controllers/challengesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import { CustomRequest } from "../middleware/validateTokenHandler";
 import TeamChallenge from "../models/teamChallengeModel";
+import TeamMember from "../models/teamMemberModel";
 
 //@desc Get Challenges
 //@route GET /api/challenges
@@ -9,11 +10,23 @@ import TeamChallenge from "../models/teamChallengeModel";
 export const getAllChallenges = asyncHandler(
   async (req: CustomRequest, res: Response) => {
     try {
-      const challenges = await TeamChallenge.find({});
+      let challenges: any = [];
 
+      if (req.query.userId) {
+        const teamMembers = await TeamMember.find({ user_id: req.query.userId })
+        const teamIds:string[] = []
+        teamMembers.map((member:any) => {
+          teamIds.push(member.team_id)
+        })
+        challenges = await TeamChallenge.find({team_id: { $in: teamIds }})
+      } else {
+        challenges = await TeamChallenge.find({});
+      }
+      
       res.status(200).json({ message: "successful", data: challenges });
     } catch (error: any) {
-      console.log(error);
+      res.status(400)
+      throw new Error(error)
     }
   }
 );
