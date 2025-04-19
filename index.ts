0de7d7f64ee7cccc473e112b739cacd04e13a957
Paul Otieno <vitalispaul48@live.com>
2025-04-19 15:10:50 +0300

    Update Origin

--- index.ts
+++ index.ts
@@ -54,7 +54,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO and attach it to the HTTP server
 export const io = new Server(server, {
   cors: {
-    origin: `http://localhost:5173`, // update to your frontend URL
+    origin: `http://localhost:5173` || "*", // update to your frontend URL
     methods: ["GET", "POST", "PATCH"],
   },
 });
