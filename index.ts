88726a7ccc2562a9e60959fb3c483c942dd45563
Paul Otieno <vitalispaul48@live.com>
2025-06-01 03:54:31 +0300

    Remove * from io config

--- index.ts
+++ index.ts
@@ -56,7 +56,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO and attach it to the HTTP server
 export const io = new Server(server, {
   cors: {
-    origin: `http://localhost:5173` || "*", // update to your frontend URL
+    origin: `http://localhost:5173`,
     methods: ["GET", "POST", "PATCH"],
   },
 });
