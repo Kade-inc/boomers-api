510ebd9b3df049d618a6f4b6d0cec98e0764f9e8
Paul Otieno <vitalispaul48@live.com>
2025-06-03 04:50:07 +0300

    Update token expiry time

--- helpers/jwtHelper.ts
+++ helpers/jwtHelper.ts
@@ -13,7 +13,7 @@ export const signAccessToken = (user:any) => {
       },
       process.env.ACCESS_TOKEN_SECRET!,
       {
-        expiresIn: "1y",
+        expiresIn: "1h",
         audience: user.id
       }
     );
@@ -30,7 +30,7 @@ export const signRefreshToken = (user:any) => {
       },
       process.env.REFRESH_TOKEN_SECRET!,
       {
-        expiresIn: "1y",
+        expiresIn: "7d",
         audience: user.id
       }
     );
