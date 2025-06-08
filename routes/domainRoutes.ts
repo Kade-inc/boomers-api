504887fe858bbc0ecda8c5f0c808cc6be5e647b5
Paul Otieno <vitalispaul48@live.com>
2025-06-08 05:56:15 +0300

    Tie topics to subdomains

--- routes/domainRoutes.ts
+++ routes/domainRoutes.ts
@@ -2,15 +2,129 @@ import express from "express";
 
 import validateToken from "../middleware/validateTokenHandler";
 
-import { getAllDomains, getAllSubDomains, getDomainTopics } from "../controllers/domainController";
+import { getAllDomains, getAllSubDomains, getDomainTopics, addDomain, addSubDomain, addDomainTopic, getDomain, getSubDomain, getDomainTopic, updateDomain, updateSubDomain, updateDomainTopic, deleteDomain, deleteSubDomain, deleteDomainTopic } from "../controllers/domainController";
 
 const domainRouter = express.Router();
 domainRouter.use(validateToken);
 
 domainRouter.get("/", getAllDomains);
+domainRouter.get("/:id", getDomain);
+domainRouter.put("/:id", updateDomain);
+domainRouter.delete("/:id", deleteDomain);
 
 domainRouter.get("/:id/subdomains", getAllSubDomains);
+domainRouter.get("/subdomains/:id", getSubDomain);
+domainRouter.put("/subdomains/:id", updateSubDomain);
+domainRouter.delete("/subdomains/:id", deleteSubDomain);
 
 domainRouter.get("/domainTopics", getDomainTopics);
+domainRouter.get("/domainTopics/:id", getDomainTopic);
+domainRouter.put("/domainTopics/:id", updateDomainTopic);
+domainRouter.delete("/domainTopics/:id", deleteDomainTopic);
+
+/**
+ * @openapi
+ * '/api/domains':
+ *  post:
+ *     tags:
+ *     - Domain Controller
+ *     summary: Add domain
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - name
+ *            properties:
+ *              name:
+ *                type: string
+ *                default: thefunky Bunch   
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+domainRouter.post("/", addDomain);
+
+/**
+ * @openapi
+ * '/api/domains/:id/subdomain':
+ *  post:
+ *     tags:
+ *     - Domain Controller
+ *     summary: Add subdomain
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - name
+ *            properties:
+ *              name:
+ *                type: string
+ *                default: thefunky Bunch
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+domainRouter.post("/:id/subdomain", addSubDomain);
+
+/**
+ * @openapi
+ * '/api/domains/topics':
+ *  post:
+ *     tags:
+ *     - Domain Controller
+ *     summary: Add domain topic
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - name
+ *            properties:
+ *              name:
+ *                type: string
+ *                default: thefunky Bunch
+ *              audience:
+ *                type: array
+ *                default: ['Developers', 'Farmers']
+ *              category:
+ *                type: array
+ *                default: ['Software']
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+domainRouter.post("/domainTopic", addDomainTopic);
 
 export default domainRouter;
