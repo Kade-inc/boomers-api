1d502be57b8e42d7fb6cfac316b54dda1a3a97a7
Paul Otieno <vitalispaul48@live.com>
2025-07-20 22:23:31 +0300

    Remove console.log

--- controllers/recommendations.ts
+++ controllers/recommendations.ts
@@ -8,40 +8,39 @@ import { Response } from "express";
 
 //access private
 export const getTeamRecommendations = asyncHandler(
-    async (req: CustomRequest, res: Response) => {
-      try {
-        const userId = req.user.id;
-        console.log("USER ID: ", userId)
-        const userProfile: any = await UserProfile.findOne({ user_id: userId });
-  
-        let teams: any = [];
-        // Safely check if interests, subDomains, and subTopics exist
-        const interests = userProfile?.interests ?? {};
-        const subdomains = interests.subdomain ?? [];
-        const domainTopics = interests.domainTopics ?? [];
-        const domains = interests.domain ?? [];
-  
-        if (Object.keys(interests).length > 0) {
-          if (subdomains.length > 0) {
-            if (domainTopics.length > 0) {
-              teams = await Team.find({
-                subdomainTopics: { $in: domainTopics},
-              });
-            } else {
-              teams = await Team.find({
-                subdomain: { $in: subdomains },
-              });
-            }
+  async (req: CustomRequest, res: Response) => {
+    try {
+      const userId = req.user.id;
+      const userProfile: any = await UserProfile.findOne({ user_id: userId });
+
+      let teams: any = [];
+      // Safely check if interests, subDomains, and subTopics exist
+      const interests = userProfile?.interests ?? {};
+      const subdomains = interests.subdomain ?? [];
+      const domainTopics = interests.domainTopics ?? [];
+      const domains = interests.domain ?? [];
+
+      if (Object.keys(interests).length > 0) {
+        if (subdomains.length > 0) {
+          if (domainTopics.length > 0) {
+            teams = await Team.find({
+              subdomainTopics: { $in: domainTopics },
+            });
           } else {
             teams = await Team.find({
-              domain: { $in: domains },
+              subdomain: { $in: subdomains },
             });
           }
-        } 
-  
-        res.status(200).json({ data: teams });
-      } catch (error: any) {
-        res.status(400).json({ error: error });
+        } else {
+          teams = await Team.find({
+            domain: { $in: domains },
+          });
+        }
       }
+
+      res.status(200).json({ data: teams });
+    } catch (error: any) {
+      res.status(400).json({ error: error });
     }
-  );
+  }
+);
